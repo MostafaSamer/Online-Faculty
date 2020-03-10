8cf3ab736e7325ec08680674cc3b812b8a8b863f
YahiaElkhashab <yahiaelkhashab17@gmail.com>
2020-03-11 01:04:30 +0200

    ignoring register

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['register' => false, 'password.request' => false, 'reset' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
